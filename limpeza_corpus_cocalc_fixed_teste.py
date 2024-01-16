302652d5b08a1ff8600d455bdd415a6c8d28c185
Luciana Dias de Macedo <lucianadiasdemacedo@MacBook-Pro-Luciana.local>
2024-01-16 10:57:48 -0500

    add files

---
+++ limpeza_corpus_cocalc_fixed_teste.py
@@ -0,0 +1,25 @@
+import glob
+import re
+
+def delete_lines(file_pattern):
+    filepaths = glob.glob(file_pattern)
+    for filepath in filepaths:
+        # Read the contents of the file
+        with open(filepath, 'r') as file:
+            lines = file.readlines()
+
+        # Remove extra spaces
+        lines = [re.sub(r'\s+', ' ', line) for line in lines]
+        # Remove extra spaces before period punctuations
+        # lines = [re.sub(r'\s*\.', '.', line) for line in lines] 
+
+
+        # Write the modified lines back to the file
+        with open(filepath, 'w') as file:
+            file.writelines(lines)
+
+        print(f"Lines modified in {filepath}.")
+
+
+file_pattern = '/Users/lucianadiasdemacedo/Downloads/*.txt' 
+delete_lines(file_pattern)
