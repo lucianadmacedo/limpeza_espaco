fc8345f6e006bcb139475f209c3a134cc77c6f13
Luciana Dias de Macedo <lucianadiasdemacedo@MacBook-Pro-Luciana.local>
2024-01-27 13:15:08 -0500

    add files

--- limpeza_corpus_cocalc_fixed_teste.py
+++ limpeza_corpus_cocalc_fixed_teste.py
@@ -1,25 +1,34 @@
 import glob
 import re
 
+
 def delete_lines(file_pattern):
     filepaths = glob.glob(file_pattern)
     for filepath in filepaths:
-        # Read the contents of the file
-        with open(filepath, 'r') as file:
-            lines = file.readlines()
 
-        # Remove extra spaces
-        lines = [re.sub(r'(\r|\n)(?![a-z])|(?<=[^.])(\r|\n)', ' ', line) for line in lines]
-        # Remove extra spaces before period punctuations
-        # lines = [re.sub(r'\s*\.', '.', line) for line in lines] 
+            # Read the contents of the file
+        with open(filepath, 'r') as file:
+            lines = file.read()
+
+
+        lines = re.sub('\n[a-z]', '', lines)
+        lines = re.sub('\n:', ' : ', lines)
+
+#        lines = lines.split('\n')
+            # Remove extra spaces before period punctuations
+        #        lines = [re.sub(r'\u000D\u000A', ' ', line) for line in lines] 
+#        lines = [re.sub('\n[a-z]', ' ', line) for line in lines] 
+                #lines = [re.sub(r'(\u000D\u000A\u003A)', ' : ', line) for line in lines] 
 
 
         # Write the modified lines back to the file
-        with open(filepath, 'w') as file:
-            file.writelines(lines)
+        with open(filepath.replace('.txt', 'new.txt'), 'w') as file:
+            file.write(lines)
 
         print(f"Lines modified in {filepath}.")
 
 
 file_pattern = '/Users/lucianadiasdemacedo/Downloads/limpeza_espacos/limpeza_espaco/*.txt' 
 delete_lines(file_pattern)
+
+
